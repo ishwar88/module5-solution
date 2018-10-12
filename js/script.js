748041fab28b9ed44ec2f33a8dd1221bd6ba5777
Ishwar Desai <argentina1.ish@gmail.com>
2018-10-12 11:59:28 -0400

    Quick Fix

--- js/script.js
+++ js/script.js
@@ -101,6 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
+
       var chosenCategoryShortName = chooseRandomCategory(categories);
 
 
@@ -114,8 +115,8 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-      //
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage, "randomCategoryShortName", chosenCategoryShortName);
+      var ShortName = "'" + chosenCategoryShortName.short_name + "'";
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", ShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
